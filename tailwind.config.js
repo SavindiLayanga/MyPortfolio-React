c943ec3cc06186960d3a0e36a7a2644fc2738842
SavindiLayanga <savindilayanga@gmail.com>
2024-10-03 16:42:40 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src//*.{js,ts,jsx,tsx}', // Ensure these file types match your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins:[],
+}
